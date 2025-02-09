008273ac97d536f5b4bd716447e84d0da3ba522a
MeluXina user <faithogun12@gmail.com>
2025-02-09 16:50:09 +0100

    Added preprocess_utils/, however it is included in the singularity file and docker image

---
+++ preprocess_utils/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+from .preprocess_utils import (
+    add_atpm,
+    add_exp,
+    create_peak_motif,
+    download_motif,
+    get_motif,
+    query_motif,
+)
---
+++ preprocess_utils/setup.py
@@ -0,0 +1,9 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='preprocess_utils',
+    version='0.1',
+    packages=find_packages(),  # This will automatically find your preprocess_utils package
+)
